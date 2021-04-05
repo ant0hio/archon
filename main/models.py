0a23329ebd931a8031aed68ee4ee601c4ce3b18d
ant0hio <vlanti@mail.ru>
2021-04-05 18:11:26 +0300

    deploy

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.validators import MinValueValidator
+import os
