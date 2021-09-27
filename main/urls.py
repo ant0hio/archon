46bd45bd1eafaf0388856bda71750408fa6b6798
ant0hio <vlanti@mail.ru>
2021-09-27 21:15:30 +0300

    deploy

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import *
 
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('foto', views.foto, name='foto'),
+    path('login/', LoginUser.as_view(), name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import *
+from .models import *
+from .utils import *
 # Create your views here.
 
 
@@ -8,3 +19,14 @@ def index(request):
 
 def foto(request):
     return render(request, 'main/foto.html')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'main/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
