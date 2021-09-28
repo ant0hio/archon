f6b5e7da953ed2f599fc2a155a566907a4308be3
ant0hio <vlanti@mail.ru>
2021-09-28 10:34:10 +0300

    deploy

--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .forms import *
 from .models import *
@@ -18,6 +19,7 @@ from django.contrib.auth import logout, login
 def index(request):
     return render(request, 'main/index.html')
 
+@login_required
 def foto(request):
     return render(request, 'main/foto.html')
 
@@ -36,6 +38,6 @@ class LoginUser(DataMixin, LoginView):
 
 def logout_user(request):
     logout(request)
-    return redirect('home')
+    return redirect('login')
 
 
