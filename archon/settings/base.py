5fb769ecbb62a00f7d9050a29a2d848e916156ab
ant0hio <vlanti@mail.ru>
2021-09-28 10:08:49 +0300

    deploy

--- archon/settings/base.py
+++ archon/settings/base.py
@@ -125,5 +125,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Redirect to home URL after login
-# LOGIN_REDIRECT_URL = '/main/'
-# LOGIN_URL = '/auth/login/'
+LOGIN_REDIRECT_URL = '/home/'
+LOGIN_URL = '/login/'
