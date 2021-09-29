69cc89f50f1cdddacf86ad3a047518c7057b8211
ant0hio <vlanti@mail.ru>
2021-09-29 09:11:58 +0300

    deploy

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views
 from .views import *
+from django_url_decr import url_decr
+from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required, permission_required
 
 
-urlpatterns = [
+#urlpatterns = patterns(''
+#    url_decr(r'^users/',
+#             include('users.urls'),
+#             decr=login_required))
+
+
+urlpatterns = (
+   # path('', url_decr(r'^foto/', include('foto.urls'), decr=login_required)),
     path('', views.index, name='home'),
     path('foto', views.foto, name='foto'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
-]
+)
