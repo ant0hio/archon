281368c877c3a202747c1f2cd31bbc9c8ca7cc2a
ant0hio <vlanti@mail.ru>
2021-11-02 12:05:17 +0300

    deploy

--- main/video.py
+++ main/video.py
@@ -1,11 +1,7 @@
 import subprocess
 from django.shortcuts import render
 
-
-
-
 args = ["ffmpeg", "-rtsp_transport tcp", "-i \"rtsp://user:user@192.168.100.22:554/profile0\"","-c copy", "-an", "-movflags", "+frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov", "/home/pi/PyProjects/archon/archon/media/video.mp4"]
-#args = ["ipconfig"]
 
 
 def ffmpeg_start():
